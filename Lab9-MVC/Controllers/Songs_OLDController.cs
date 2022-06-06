1786f7ddc3ca1d866a7b7fbbbe1a13b2d062f532
maciekluczak <maciekluczak2000@gmail.com>
2022-06-07 01:23:11 +0200

    Dodaj pliki projektów.

---
+++ Lab9-MVC/Controllers/Songs_OLDController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab9_MVC.Controllers
+{
+    public class Songs_OLDController : Controller
+    {
+        // GET: Songs
+        public ActionResult Index()
+        {
+            Models.Song song = new Models.Song();
+            song.Id = 1;
+            song.Name = "Utwór 1";
+            song.Artist = "Wykonawca 1";
+            song.GenreId = 1;
+            return View(song);
+        }
+
+        public ActionResult Square(int? id)
+        {
+            if (id == null)
+            {
+                return Content("Brak parametru");
+            }
+            else
+            {
+                return Content((id * id).ToString());
+            }
+        }
+    }
+}
---
+++ Lab9-MVC/Models/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab9_MVC.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Artist { get; set; }
+
+        public int GenreId{ get; set; }
+
+    }
+}
